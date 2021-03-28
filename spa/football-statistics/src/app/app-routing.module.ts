79ee520a34b5dc42717984be59abbc589600ae54
Alejandro Carmona <acarmonaa@colaborador.planeta.es>
2021-03-28 11:42:26 +0200

    adding spa

--- spa/football-statistics/src/app/app-routing.module.ts
+++ spa/football-statistics/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { YellowcardListComponent } from './components/yellowcard-list/yellowcard-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'statistics/yellowcard', component : YellowcardListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
